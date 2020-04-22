8906fb1454bf9dc57d2e26d9c5d83b31153d467d
Ivan Dubrovin <io.dubrovin@icloud.com>
2020-04-22 18:45:18 +0300

    import Eigen and spdlog libraries

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
+#include "spdlog/spdlog.h"
+#include "Eigen/Dense"
 #include "medium.h"
 
 int main() {
+    spdlog::set_pattern("[%L][%H:%M:%S] %v");
+    spdlog::info("spdlog imported successfully!");
+
+    Eigen::Matrix2d test;
+    test << 0, 1,
+            2, 3;
+    std::cout << test << std::endl;
+
     auto M = Medium(1200);
     M.Layers.emplace_back(200, 10);
 
